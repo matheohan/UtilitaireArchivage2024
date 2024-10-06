e059bdcad3307b859bab31ea5ad7a7b6525c2272
Mathéo Han <vfxdiking@gmail.com>
2024-10-06 22:01:47 +0200

    - Fix d'une typo dans le fichier config.json. - Fix des typos dans le fichier main.py et simplification de celui-ci.

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ try:
     if not os.path.exists(f"./{config['files_directory_name']}"):
         os.makedirs(f"./{config['files_directory_name']}")
 
-    download_zip_file(config['remote_host_server_url'] + "/" + config['remote_file_name'], "./files/" + config['remote_file_name'])
+    file_path = f"./{config['files_directory_name']}/{config['remote_file_name']}"
+    download_zip_file(f"{config['remote_host_server_url']}/{config['remote_file_name']}", file_path)
     
-
     # 2. Décompression et vérification du contenu
-    extract_zip(f"./{config['files_directory_name']}/{config['remote_file_name']}", config['extracted_file_name'])
+    extract_zip(file_path, config['extracted_file_name'])
 
     # 3. Calcul du hash du fichier SQL extrait
     extracted_file_path = f"./{config['files_directory_name']}/{config['extracted_file_name'].split('/')[-1]}"
@@ -42,17 +42,17 @@ try:
         save_hash(current_hash, hash_file_path)
 
         # 5. Création de l'archive
-        archive_name = current_date = f"{datetime.now().strftime('%Y%m%d')}.tgz"
+        archive_name = f"{datetime.now().strftime('%Y%m%d')}.tgz"
         create_tgz_archive(extracted_file_path, archive_name)
 
         # 6. Transfert de l'archive vers le serveur distant
         if config['server']['type'] == 'sftp':
             upload_to_sftp(archive_name, config['server'])
     else:
-        logging.info("Le fichier est identique à celui de la veille. Fin du script.")
+        logging.info("Le fichier est identique à celui de la veille.")
 
     # 7. Gestion de la durée de rétention
-    clean_old_archives(config['server'], config['retention_days'])
+    clean_old_archives(config['retention_days'], config['server'], )
 
 except Exception as e:
     logging.error(f"Erreur lors de l'exécution : {e}")
